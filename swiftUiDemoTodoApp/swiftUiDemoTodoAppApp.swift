637a40b70ea381f32caef8072f14915148d0ae05
mikayilsrt <sert.mikayil@gmail.com>
2020-09-30 20:37:58 +0200

    Initial Commit

---
+++ swiftUiDemoTodoApp/swiftUiDemoTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUiDemoTodoAppApp.swift
+//  swiftUiDemoTodoApp
+//
+//  Created by Mikayil SERT on 30/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct swiftUiDemoTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
